5102e685fc6cfb850a82fd0e9667bfdee469d815
smoumita19 <smoumita19@yahoo.com>
2023-06-11 08:50:02 +0530

    Uploaded solved problem 175 and 181

---
+++ Problems/181. Employees Earning/solution.sql
@@ -0,0 +1,32 @@
+# 181. Employees Earning More Than Their Managers
+
+# Table: Employee
+
+# +-------------+---------+
+# | Column Name | Type    |
+# +-------------+---------+
+# | id          | int     |
+# | name        | varchar |
+# | salary      | int     |
+# | managerId   | int     |
+# +-------------+---------+
+# id is the primary key column for this table.
+# Each row of this table indicates the ID of an employee, their name, salary, and the ID of their manager.
+
+
+# Write an SQL query to find the employees who earn more than their managers.
+
+# Return the result table in any order.
+
+# The query result format is in the following example.
+
+Solutions
+===============================================================================================================
+
+# Write your MySQL query statement below
+
+SELECT A.name Employee
+FROM EMPLOYEE A
+INNER JOIN EMPLOYEE B
+ON A.managerId = B.id
+WHERE A.salary > B.salary;
