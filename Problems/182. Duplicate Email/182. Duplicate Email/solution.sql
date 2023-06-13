2fe4485cf35f86833ea7f2cf99da090283ec1508
smoumita19 <smoumita19@yahoo.com>
2023-06-13 20:24:03 +0530

    Uploaded solved problems of 183, 196, 197

--- Problems/182. Duplicate Email/182. Duplicate Email/solution.sql
+++ Problems/182. Duplicate Email/182. Duplicate Email/solution.sql
@@ -17,6 +17,25 @@
 --
 -- The query result format is in the following example.
 
+--Example 1:
+
+--Input: 
+--Person table:
+--+----+---------+
+--| id | email   |
+--+----+---------+
+--| 1  | a@b.com |
+--| 2  | c@d.com |
+--| 3  | a@b.com |
+--+----+---------+
+--Output: 
+--+---------+
+--| Email   |
+--+---------+
+--| a@b.com |
+--+---------+
+--Explanation: a@b.com is repeated two times.
+
 Solutions
 ===============================================================================================================
 
